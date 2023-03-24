85f015fc29711b3997f60b7666e35777e999cfaf
MighyCode <maxence.bazin@bordeaux-inp.fr>
2023-03-24 11:27:09 +0100

    push two action id

--- AfficheurPoisson/src/ProjetPoisson/project/lib/ActionId.java
+++ AfficheurPoisson/src/ProjetPoisson/project/lib/ActionId.java
@@ -6,6 +6,10 @@ public class ActionId {
     public static final int MENU_NEXT_COMMAND = InputManager.getAndIncrementId();
     public static final int TEXT_INPUT_LEFT_DELETE = InputManager.getAndIncrementId();
     public static final int TEXT_INPUT_RIGHT_DELETE = InputManager.getAndIncrementId();
+
+    public static final int TEXT_CURSOR_LEFT = InputManager.getAndIncrementId();
+    public static final int TEXT_CURSOR_RIGHT = InputManager.getAndIncrementId();
+
     public static final int SHIFT_MODIFIER = InputManager.getAndIncrementId();
     public static final int ALT_MODIFIER = InputManager.getAndIncrementId();
     public static final int ENTER_COMMAND = InputManager.getAndIncrementId();
