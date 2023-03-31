88307342682f78b4821474716eb19cfbca4e8e12
Louis-Victor Ladagnous <louivictor@live.fr>
2023-03-31 11:39:55 +0200

    New tests and new makefile

---
+++ Controller/test_fish.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "fish.h"
+
+int main(){
+    struct fish * fish = malloc(sizeof(struct fish));
+
+    fish_init(fish);
+
+    int size[2] = {10, 5};
+    int coords[2] = {200, 200}; 
+
+    fish_create(fish, coords, size, "Nathan", NULL);
+    fish_print(fish);
+
+    free(fish);
+
+    return 0;
+}
