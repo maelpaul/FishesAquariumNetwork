e4006598ce1fdf9b4778ade2ea1ded4e4c19eb59
Nicolas Przybylski <nicolas.przybylski@bordeaux-inp.fr>
2023-03-31 11:38:05 +0200

    added print in view

--- Controller/view.c
+++ Controller/view.c
@@ -1,6 +1,7 @@
 #include "view.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 
 void view_init(struct view * view){
     for(int i=0; i<2; i++){
@@ -16,4 +17,12 @@ void view_create(struct view * view, int * coords, int * size, char * name){
         view->size[i]=size[i];
     }
     view->name=name;
+}
+
+void view_print(struct view * view){
+    printf("============ Debug ============\n");
+    printf("view name   : %s\n", view->name);
+    printf("view coords : (%d,%d)\n", view->coords[0],view->coords[1]);
+    printf("view size   : (%d,%d)\n", view->size[0],view->size[1]);
+    printf("===============================\n");
 }
