2a4a34512f4aee1334b49cca879ca69a83be7c69
Maël PAUL <maelpaul@orange.fr>
2023-03-31 11:26:13 +0200

    yolo

--- Controller/fish.c
+++ Controller/fish.c
@@ -9,9 +9,22 @@ void fish_init(struct fish * fish) {
         fish->coords[i] = 0;
         fish->size[i] = 0;
     } 
-
 }
 
-void fish_create(struct fish * fish, int coords[2], int size[2], char * name, void (*path)(struct fish *)) {
+void fish_create(struct fish * fish, int * coords, int * size, char * name, void (*path)(struct fish *)) {
+    fish->name = name;
+    fish->path = path;
+    for (int i = 0; i < 2; ++i) {
+        fish->coords[i] = coords[i];
+        fish->size[i] = size[i];
+    } 
+}
 
+void fish_print(struct fish * fish) {
+    printf("%s\n", fish->name);
+    printf("%p\n", fish->path);
+    for (int i = 0; i < 2; ++i) {
+        printf("%d\n", fish->coords[i]);
+        printf("%d\n", fish->size[i]);
+    } 
 }
