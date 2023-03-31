75513680c660a5c88d5671d1a3b81af6a9a4a599
Maël PAUL <maelpaul@orange.fr>
2023-03-31 16:56:55 +0200

    bug

--- Controller/test/test_fish.c
+++ Controller/test/test_fish.c
@@ -1,18 +1,24 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "../src/fish.h"
 
+void RandomWayPoint(struct fish * fish) {
+    fish->dest[0] = rand() % 100;
+    fish->dest[1] = rand() % 100;    
+}
+
 int main(){
     struct fish * fish = malloc(sizeof(struct fish));
+
+    void (*path)(struct fish *) = &RandomWayPoint;
 
     fish_init(fish);
 
     int size[2] = {10, 5};
     int coords[2] = {200, 200};
 
-    fish_create(fish, coords, size, "Nathan", NULL);
+    fish_create(fish, coords, size, "Nathan", path);
     fish_print(fish);
 
     free(fish);
