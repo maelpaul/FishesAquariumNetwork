2aa88ae05be0b9abd0d54b7f79adc6e60eb0ff04
Nicolas Przybylski <nicolas.przybylski@bordeaux-inp.fr>
2023-04-07 11:29:41 +0200

    sdq

--- Controller/src/server.c
+++ Controller/src/server.c
@@ -7,39 +7,7 @@
 #include <string.h>
 #include <errno.h>
 
-struct config {
-    int controller_port;
-    int display_timeout_value;
-    int fish_update_interval;
-};
-
-int load_config(const char *filename, struct config *conf)
-{
-    FILE *fp;
-    char line[256], key[256], value[256];
-
-    fp = fopen(filename, "r");
-    if (!fp) {
-        fprintf(stderr, "Failed to open %s\n", filename);
-        return -1;
-    }
-
-    while (fgets(line, sizeof(line), fp)) {
-        if (sscanf(line, "%255s = %255s", key, value) == 2) {
-            if (strcmp(key, "controller-port") == 0) {
-                conf->controller_port = atoi(value);
-            } else if (strcmp(key, "display-timeout-value") == 0) {
-                conf->display_timeout_value = atoi(value);
-            } else if (strcmp(key, "fish-update-interval") == 0) {
-                conf->fish_update_interval = atoi(value);
-            }
-        }
-    }
-
-    fclose(fp);
-
-    return 0;
-}
+#include "server_utils.h"
 
 int main()
 {
--- Controller/src/server_utils.h
+++ Controller/src/server_utils.h
@@ -3,3 +3,5 @@ struct config {
     int display_timeout_value;
     int fish_update_interval;
 };
+
+int load_config(const char *filename, struct config *conf);
