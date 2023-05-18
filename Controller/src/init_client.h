cbb894ead970398b69371f7bb1380ea7a695b244
Nicolas Przybylski <nicolas.przybylski@bordeaux-inp.fr>
2023-05-18 17:31:49 +0200

    finished fix conflicts

--- Controller/src/init_client.h
+++ Controller/src/init_client.h
@@ -1,7 +1,3 @@
 #include "server_utils.h"
 
-<<<<<<< HEAD
-int init_client(char * header, char * buffer, struct aquarium * aquarium, pthread_mutex_t * mutex, int client_id);
-=======
-int init_client(char * buffer, struct aquarium * aquarium, pthread_mutex_t * mutex, int client_id, struct view * client_view);
->>>>>>> 7243e527b3908e0f9a579d092f4bb72447d72d91
+int init_client(char * header, char * buffer, struct aquarium * aquarium, pthread_mutex_t * mutex, int client_id, struct view * client_view);
